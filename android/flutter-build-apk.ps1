d5e21078b39f0f44348f6f00b10f06252758fe4e
iGoX <code@igox.org>
2026-03-20 18:11:25 +0100

    [Android] Fix helper build scripts

--- android/flutter-build-apk.ps1
+++ android/flutter-build-apk.ps1
@@ -8,13 +8,16 @@ flutter build apk --$buildType
 $oldApk = "$apkDir\app-$buildType.apk"
 $newApk = "$apkDir\$baseName-$buildType.apk"
 if (Test-Path $oldApk) {
+    if (Test-Path $newApk) { Remove-Item $newApk -Force }
     Rename-Item -Path $oldApk -NewName "$baseName-$buildType.apk"
     Write-Host "APK renamed to: $baseName-$buildType.apk"
 }
 
 # Rename SHA1 (if exists)
 $oldSha1 = "$apkDir\app-$buildType.apk.sha1"
+$newSha1 = "$apkDir\$baseName-$buildType.apk.sha1"
 if (Test-Path $oldSha1) {
+    if (Test-Path $newSha1) { Remove-Item $newSha1 -Force }
     Rename-Item -Path $oldSha1 -NewName "$baseName-$buildType.apk.sha1"
     Write-Host "SHA1 renamed to: $baseName-$buildType.apk.sha1"
 }
--- android/flutter-build-apk.sh
+++ android/flutter-build-apk.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 BUILD_TYPE=${1:-debug}
 APK_DIR="../build/app/outputs/flutter-apk"
 BASE_NAME="org.igox.apps.android.busylight-buddy"
@@ -10,6 +9,7 @@ flutter build apk --$BUILD_TYPE
 OLD_APK="$APK_DIR/app-$BUILD_TYPE.apk"
 NEW_APK="$APK_DIR/$BASE_NAME-$BUILD_TYPE.apk"
 if [ -f "$OLD_APK" ]; then
+    [ -f "$NEW_APK" ] && rm -f "$NEW_APK"
     mv "$OLD_APK" "$NEW_APK"
     echo "APK renamed to: $BASE_NAME-$BUILD_TYPE.apk"
 fi
@@ -18,6 +18,7 @@ fi
 OLD_SHA1="$APK_DIR/app-$BUILD_TYPE.apk.sha1"
 NEW_SHA1="$APK_DIR/$BASE_NAME-$BUILD_TYPE.apk.sha1"
 if [ -f "$OLD_SHA1" ]; then
+    [ -f "$NEW_SHA1" ] && rm -f "$NEW_SHA1"
     mv "$OLD_SHA1" "$NEW_SHA1"
     echo "SHA1 renamed to: $BASE_NAME-$BUILD_TYPE.apk.sha1"
 fi
