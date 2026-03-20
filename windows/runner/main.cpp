b1fec8737871aae57cf9fb31e1be0e36beeac2ab
iGoX <code@igox.org>
2026-03-20 13:37:18 +0100

    Windows APP: send APP in icon tray rather than task bar

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,38 +1,102 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+#include <shellapi.h>
 
 #include "flutter_window.h"
 #include "utils.h"
 
+#define WM_TRAYICON (WM_USER + 1)
+#define ID_TRAY_EXIT 1001
+#define ID_TRAY_SHOW 1002
+
+NOTIFYICONDATA nid = {};
+HWND g_hwnd = nullptr;
+
+void AddTrayIcon(HWND hwnd) {
+  nid.cbSize = sizeof(NOTIFYICONDATA);
+  nid.hWnd = hwnd;
+  nid.uID = 1;
+  nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+  nid.uCallbackMessage = WM_TRAYICON;
+  nid.hIcon = LoadIcon(GetModuleHandle(nullptr), MAKEINTRESOURCE(IDI_APP_ICON));
+  wcscpy_s(nid.szTip, L"BusyLight Buddy");
+  Shell_NotifyIcon(NIM_ADD, &nid);
+}
+
+void RemoveTrayIcon() {
+  Shell_NotifyIcon(NIM_DELETE, &nid);
+}
+
+void ShowTrayMenu(HWND hwnd) {
+  HMENU menu = CreatePopupMenu();
+  AppendMenu(menu, MF_STRING, ID_TRAY_SHOW, L"Show");
+  AppendMenu(menu, MF_SEPARATOR, 0, nullptr);
+  AppendMenu(menu, MF_STRING, ID_TRAY_EXIT, L"Quit");
+  POINT pt;
+  GetCursorPos(&pt);
+  SetForegroundWindow(hwnd);
+  TrackPopupMenu(menu, TPM_BOTTOMALIGN | TPM_LEFTALIGN, pt.x, pt.y, 0, hwnd, nullptr);
+  DestroyMenu(menu);
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
+                      _In_ wchar_t* command_line, _In_ int show_command) {
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
-
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
-
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(420, 820);
-  if (!window.Create(L"BusyLight Buddy", origin, size)) {
+  if (!window.CreateAndShow(L"BusyLight Buddy", origin, size)) {
     return EXIT_FAILURE;
   }
-  window.SetQuitOnClose(true);
 
-  ::MSG msg;
+  g_hwnd = window.GetHandle();
+  AddTrayIcon(g_hwnd);
+
+  // Subclass the window to intercept WM_SYSCOMMAND and WM_TRAYICON
+  SetWindowSubclass(g_hwnd, [](HWND hwnd, UINT msg, WPARAM wp, LPARAM lp,
+                                UINT_PTR, DWORD_PTR) -> LRESULT {
+    switch (msg) {
+      case WM_SYSCOMMAND:
+        if ((wp & 0xFFF0) == SC_MINIMIZE) {
+          ShowWindow(hwnd, SW_HIDE);  // hide instead of minimize
+          return 0;
+        }
+        break;
+      case WM_TRAYICON:
+        if (lp == WM_LBUTTONDBLCLK || lp == WM_LBUTTONUP) {
+          ShowWindow(hwnd, SW_SHOW);
+          SetForegroundWindow(hwnd);
+        } else if (lp == WM_RBUTTONUP) {
+          ShowTrayMenu(hwnd);
+        }
+        break;
+      case WM_COMMAND:
+        if (LOWORD(wp) == ID_TRAY_EXIT) {
+          RemoveTrayIcon();
+          PostQuitMessage(0);
+        } else if (LOWORD(wp) == ID_TRAY_SHOW) {
+          ShowWindow(hwnd, SW_SHOW);
+          SetForegroundWindow(hwnd);
+        }
+        break;
+      case WM_DESTROY:
+        RemoveTrayIcon();
+        break;
+    }
+    return DefSubclassProc(hwnd, msg, wp, lp);
+  }, 1, 0);
+
+  ::MSG msg = {};
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
